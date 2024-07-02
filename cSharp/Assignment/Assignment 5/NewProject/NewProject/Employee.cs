e814cc8e9011cd88cab87378e3ff9bfb710a4df4
Aman Ullah <amanu@infinite.com>
2024-07-02 11:28:23 +0530

    Assignment 4

---
+++ cSharp/Assignment/Assignment 5/NewProject/NewProject/Employee.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewProject
+{
+    //This is my base Class
+    public class Emp
+    {
+        public int Empid { get; set; }
+        public string Empname { get; set; }
+        public float Salary { get; set; }
+
+        // Constructor to initialize the properties
+        public Emp(int empid, string empname, float salary)
+        {
+            Empid = empid;
+            Empname = empname;
+            Salary = salary;
+        }
+        // Method to display employee details
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"Empid: {Empid}, Empname: {Empname}, Salary: {Salary}");
+        }
+    }
+    // Derived class ParttimeEmployee inheriting from Employee
+    public class ParttimeEmployee : Emp
+    {
+        // Property for Wages
+        public int Wages { get; set; }
+
+        //Constructor to initialise the properties of both base and derived classes
+        public ParttimeEmployee(int empid, string empname, float salary, int wages) : base(empid, empname, salary) //I am using constructor chaining here.
+        {
+            Wages = wages;
+        }
+        // Method to display part-time employee details including wages
+        public new void DisplayDetails()
+        {
+            base.DisplayDetails();
+            Console.WriteLine($"Wages: {Wages}");
+        }
+    }
+    class Employee
+    {
+        public static void Main()
+        {
+            // Creating a ParttimeEmployee object
+            ParttimeEmployee parttimeEmp = new ParttimeEmployee(07, "Amaan Ullah", 2500.0f, 5000);
+
+            // Displaying the details of the part-time employee
+            parttimeEmp.DisplayDetails();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ cSharp/Assignment/Assignment 5/NewProject/NewProject/Test.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewProject
+{
+    public class Box
+    {
+        public double Length { get; set; }
+        public double Breadth { get; set; }
+        public Box(double length, double breadth)
+        {
+            Length = length;
+            Breadth = breadth;
+        }
+        public static Box Add(Box box1, Box box2)
+        {
+            double newLength = box1.Length + box2.Length;
+            double newBreadth = box1.Breadth + box2.Breadth;
+            return new Box(newLength, newBreadth);
+        }
+        public void DisplayDimensions()
+        {
+            Console.WriteLine($"Length: {Length}, Breadth: {Breadth}");
+        }
+    }
+    
+    public class Test
+    {
+        public static void Main()
+        {
+            Box box1 = new Box(3.5, 2.0);
+            Box box2 = new Box(4.5, 3.0);
+            Box box3 = Box.Add(box1, box2);
+
+            Console.WriteLine("Box 1 Dimensions:");
+            box1.DisplayDimensions();
+
+            Console.WriteLine("Box 2 Dimensions:");
+            box2.DisplayDimensions();
+
+            Console.WriteLine("Box 3 (Result of Addition) Dimensions:");
+            box3.DisplayDimensions();
+
+            Console.ReadLine();
+        }
+    }
+}
