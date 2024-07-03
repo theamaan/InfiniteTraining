0ea98f9207b339efe7ccbbe0577a0f679171b4f2
Aman Ullah <amanu@infinite.com>
2024-07-03 12:49:09 +0530

    3rd CC

---
+++ cSharp/Assessment/Code Challenge 3/Challenge/Challenge/Box.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge
+{
+    class Box
+    {
+        private int length;
+        private int breadth;
+
+        public Box(int length, int breadth)
+        {
+            this.length = length;
+            this.breadth = breadth;
+        }
+
+        public static Box AddBoxes(Box box1, Box box2)
+        {
+            int newLength = box1.length + box2.length;
+            int newBreadth = box1.breadth + box2.breadth;
+            return new Box(newLength, newBreadth);
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Length: " + length);
+            Console.WriteLine("Breadth: " + breadth);
+        }
+    }
+
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Box box1 = new Box(50, 10);
+            Box box2 = new Box(150, 20);
+
+            Box box3 = Box.AddBoxes(box1, box2);
+
+            Console.WriteLine("Details of the third box (after addition):");
+            box3.Display();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ cSharp/Assessment/Code Challenge 3/Challenge/Challenge/Cricket.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge
+{
+    class Cricket
+    {
+        private static List<int> scores = new List<int>();
+
+        public static void pointsCalculation(int no_of_matches)
+        {
+            for (int i = 0; i < no_of_matches; i++)
+            {
+                Console.Write("Enter score for match " + (i + 1) + ": ");
+                int score = int.Parse(Console.ReadLine());
+                scores.Add(score);
+            }
+        }
+
+        public static void display(int no_of_matches)
+        {
+            int sum = scores.Sum();
+            double average = (double)sum / no_of_matches;
+
+            Console.WriteLine("Total Sum of Scores: " + sum);
+            Console.WriteLine("Average Score: " + average);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of matches: ");
+            int no_of_matches = int.Parse(Console.ReadLine());
+
+            pointsCalculation(no_of_matches);
+            display(no_of_matches);
+            Console.ReadLine();
+        }
+    }
+}
